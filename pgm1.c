fb25d86523604b36b57d602a5fc74e777e227e90
Jayaprakash-boss <50835417+Jayaprakash-boss@users.noreply.github.com>
2019-05-20 19:42:48 +0530

    Create pgm1.c

---
+++ pgm1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include<conio.h>
+void main() 
+{ 
+	int num;
+	printf("Enter the number num: "); 
+	scanf("%d", &num);
+	if(num > 0)
+  {
+	printf("%d is positive.", num); 
+	}
+  else if (num < 0) 
+	{
+  printf("%d is negative.", num); 
+	}
+  else if (num == 0) 
+	{
+  printf("%d is zero.", num); 
+	}
+  return 0; 
+} 
