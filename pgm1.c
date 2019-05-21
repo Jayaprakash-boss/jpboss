ce56d083586bb3dbab4f1b4e92dfbb20bd2a75ac
Jayaprakash-boss <50835417+Jayaprakash-boss@users.noreply.github.com>
2019-05-21 16:18:07 +0530

    Update pgm1.c

--- pgm1.c
+++ pgm1.c
@@ -6,16 +6,10 @@ void main()
 	printf("Enter the number num: "); 
 	scanf("%d", &num);
 	if(num > 0)
-  {
-	printf("%d is positive.", num); 
-	}
-  else if (num < 0) 
-	{
-  printf("%d is negative.", num); 
-	}
-  else if (num == 0) 
-	{
-  printf("%d is zero.", num); 
-	}
-  return 0; 
+	printf("%d is positive.", num);
+        else if (num < 0) 
+        printf("%d is negative.", num); 
+        else if (num == 0) 
+        printf("%d is zero.", num); 
+	return 0; 
 } 
