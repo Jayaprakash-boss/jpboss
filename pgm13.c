ca937f4b174e612b52ac31dfb1d1144a935473db
Jayaprakash-boss <50835417+Jayaprakash-boss@users.noreply.github.com>
2019-05-23 11:27:24 +0530

    Create pgm13.c

---
+++ pgm13.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void main()
+{
+int i,b,count=0;
+scanf("%d",&b);
+for(i=1;i<=b;i++)
+{
+    if(b%i==0)
+    {
+        count++;
+    }
+}
+if(count==2)
+{
+    printf("yes");
+}
+else
+{
+    printf("no");
+}
+return 0;
+}
