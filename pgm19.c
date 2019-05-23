48184a4ced9952d72cf4eda3e711853278f88cec
Jayaprakash-boss <50835417+Jayaprakash-boss@users.noreply.github.com>
2019-05-23 11:44:52 +0530

    Update pgm19.c

--- pgm19.c
+++ pgm19.c
@@ -1,18 +1,13 @@
 #include <stdio.h>
 int main()
 {
-    int i,fact=1;
-    scanf("%d",&i);
-    if(i<=0)
-    {
-     fact=1;
-     }
-     else
-     {
-         for(int i=1;i<=i;i++)
-         {
-             fact=fact*i;
-         }
-     }
-     printf("%d",fact);
+    int fact=1,i,n;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    
+        fact=fact*i;
+    
+    printf("%d ",fact);
+
+    return 0;
 }
