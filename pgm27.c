0ece46e5e666257f428930694192b4ac810a9003
Jayaprakash-boss <50835417+Jayaprakash-boss@users.noreply.github.com>
2019-05-23 13:24:18 +0530

    Create pgm27.c

---
+++ pgm27.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include<ctype.h>
+int main()
+{
+    char a;
+    scanf("%c",&a);
+    if(isdigit(a))
+    {
+    printf("yes");
+    }
+    else
+    {
+    printf("No");
+    }
+ return 0;
+}
