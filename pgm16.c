0ee51e1e85908c59574fcdec3af9052ba9eb6b2d
Jayaprakash-boss <50835417+Jayaprakash-boss@users.noreply.github.com>
2019-05-23 11:35:16 +0530

    Create pgm16.c

---
+++ pgm16.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main() 
+{
+   int i,j,a,c,count;
+   scanf("%d%d",&c,&a);
+   for(i=c+1;i<=a;i++)
+   {
+       count=0;
+       for(j=2;j<=i;j++)
+       {
+       if(i%j==0)
+       {
+           break;
+       }
+       }
+       if(j==i)
+       {
+           printf("%d ",i);
+       }
+   }
+}
