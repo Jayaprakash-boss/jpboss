7aefe143b5d39bb99617ae2b9b787d8b2d32cc8e
Jayaprakash-boss <50835417+Jayaprakash-boss@users.noreply.github.com>
2019-05-23 11:29:56 +0530

    Create pgm14.c

---
+++ pgm14.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<stdlib.h>
+void main()
+{
+ int a,b,i;
+ scanf("%d %d",&a,&b);
+ if(a>=b)
+ {
+ printf("Invalid");
+ exit(1);
+ }
+ for(i=a+1;i<b;i++)
+ {
+  if(i%2!=0)
+  {
+  printf("%d",i);
+  if(i!=(b-1))
+  printf(" ");
+  }
+ }
+}
