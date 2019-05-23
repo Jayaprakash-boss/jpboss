a1f637b1048bbfa48b6cc59314fd1c77ca86e2db
Jayaprakash-boss <50835417+Jayaprakash-boss@users.noreply.github.com>
2019-05-23 11:42:53 +0530

    Create pgm19.c

---
+++ pgm19.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    int i,fact=1;
+    scanf("%d",&i);
+    if(i<=0)
+    {
+     fact=1;
+     }
+     else
+     {
+         for(int i=1;i<=i;i++)
+         {
+             fact=fact*i;
+         }
+     }
+     printf("%d",fact);
+}
