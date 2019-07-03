e0a942472d6479eebcac5fd162e6b8af4e865cfd
Jayaprakash-boss <50835417+Jayaprakash-boss@users.noreply.github.com>
2019-07-03 23:38:29 +0530

    Update analytics.php

--- analytics.php
+++ analytics.php
@@ -6,7 +6,7 @@
 <head>
 <title> Admin Panel </title>
 <meta charset="UTF-8">
-   <meta name="Author" content="Kasirajapandian">
+   <meta name="Author" content="Jayaprakash">
  <meta name="viewport" content="width=device-width, initial-scale=1">
  <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css">
  <link rel="stylesheet" href="css/fontawesome.min.css" type="text/css">
